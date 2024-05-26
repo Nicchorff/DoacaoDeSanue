34965121ffea12c9914cc2a2dd6e460e4ad11c18
EricoAP <pedrosaerico@gmail.com>
2024-05-26 19:36:47 -0300

    fronend

---
+++ DoacaoDeSangue/Views/Home/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DoacaoDeSangue.Views.Home
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
